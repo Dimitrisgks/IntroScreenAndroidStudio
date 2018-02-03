2421c7ed466d990ef47d80013ebb149f95bd19ab
Dimitrisgks <www.dimitrisgakis@gmail.com>
2018-02-03 21:59:34 +0200

    Initial commit

---
+++ app/src/main/java/com/example/sin/intro/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.sin.intro;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    //set the screen intro time in milliseconds
+    private static int SPLASH_TIME_OUT = 4000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //FullScreen the activity(INTRO in this example) without problems
+        int currentApiVersion = Build.VERSION.SDK_INT;
+        final int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        if (currentApiVersion >= Build.VERSION_CODES.KITKAT) {
+            getWindow().getDecorView().setSystemUiVisibility(flags);
+            final View decorView = getWindow().getDecorView();
+            decorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+                @Override
+                public void onSystemUiVisibilityChange(int visibility) {
+                    if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                        decorView.setSystemUiVisibility(flags);
+                    }
+                }
+            });
+        }
+        //end of fullscreen
+        //Activity after the intro is done
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+            Intent homeIntent = new Intent(MainActivity.this,HomeActivity.class);
+            startActivity(homeIntent);
+            finish();
+            }
+        },SPLASH_TIME_OUT);
+    }}
